5d18029fb00f73c13fbb6a61b8f573d4ac4a8a11
janhavi-1111 <janhavi.a.chavan@gmail.com>
2020-07-05 19:28:27 +0530

    first

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,24 @@
 
+x<-rbind(x_train,x_test)
+Y<-rbind(y_train,y_test)
+subject<-rbind(subject_train,subject_test)
+Merged_Data<-cbind(subject,Y,x)
+TidyData<-Merged_Data%>%select(subject,code,contains("mean"),contains("std"))
+TidyData$code<-activities[TidyData$code,2]
+names(TidyData)[2]="activity"
+names(TidyData)<-gsub("Acc","Accelerometer",names(TidyData))
+names(TidyData)<-gsub("Gyro","Gyroscope",names(TidyData))
+names(TidyData)<-gsub("BodyBody","Body",names(TidyData))
+names(TidyData)<-gsub("Mag","Magnitude",names(TidyData))
+names(TidyData)<-gsub("^t","Time"names(TidyData))
+names(TidyData)<-gsub("^f","Frequency",names(TidyData))
+names(TidyData)<-gsub("tBody","TimeBody",names(TidyData))
+names(TidyData)<-gsub("-mean()","Mean",names(TidyData),ignore.case=TRUE)
+names(TidyData)<-gsub("-std()","STD",names(TidyData),ignore.case=TRUE)
+names(TidyData)<-gsub("-freq()","Frequency",names(TidyData),ignore.case=TRUE)
+names(TidyData)<-gsub("angle","Angle",names(TidyData))
+names(TidyData)<-gsub("gravity","Gravity",names(TidyData))
+FinalData<-TidyData%>%
+  group_by(subject,activity)%>%
+  summarise_all(funs(mean))
+write.table(FinalData,"FinalData.txt",row.name=FALSE)
